62cd9340f5c0a92f572300cd61aa6d9e26150a6c
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-20 12:07:09 +0300

    init: first commit

---
+++ HelloAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloAlamofire
+//
+//  Created by Egor Gorskikh on 20.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+    }
+
+
+}
+
