8cf60f96c7840a42e9515e84eb8b2d5418d9fa48
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-21 15:07:46 +0300

    feat: get JSON via Alamofire

---
+++ HelloAlamofire/Greeting.swift
@@ -0,0 +1,13 @@
+//
+//  Greeting.swift
+//  HelloAlamofire
+//
+//  Created by Egor Gorskikh on 20.08.2021.
+//
+
+import Foundation
+
+struct Greeting: Decodable {
+    let text: String
+}
+
--- HelloAlamofire/ViewController.swift
+++ HelloAlamofire/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlet
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - Property
+    private let urlGreeting = "https://egorskikh.ru/json/greeting.json"
+    private var arrayLabel: [Greeting] = []
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+
+        fetchGreeting()
+    }
+
+    // MARK: - Private method
+
+    private func fetchGreeting() {
+
+        Session.default.request(urlGreeting).responseDecodable(of: [Greeting].self) { [self] response in
+            switch response.result {
+            case .success(let users):
+                print(users)
+                arrayLabel = users
+                tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrayLabel.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let greeting = arrayLabel[indexPath.item]
+        cell.textLabel?.text = greeting.text
+        return cell
     }
 
 
 }
-
